54ff76321bb071caab98d6bb80946a06dfa1e0a4
syatch <sket.dance.again@gmail.com>
2018-03-16 06:18:21 +0900

    ROSのプログラムがきちんとpushできていなかったのでpushし直し

--- Mecanum_wheel_ROS/multip/src/multip.cpp
+++ Mecanum_wheel_ROS/multip/src/multip.cpp
@@ -28,21 +28,21 @@ int main(int argc, char **argv)
     {
 //std_msgs(スタンダードメッセージ)のうち、Int32MultiArray型のデータ（変数あるいは枠）のarrayを使うよっていう宣言。ROSにおける情報のやり取りをするときの特殊な変数的な何か。型は色々ある
         std_msgs::Int32MultiArray array;
-        //Clear arrayのデータの初期化
+//	Clear arrayのデータの初期化
         array.data.clear();
-　　　　　　　　//array.data.push_backでarrayデータの最後尾にデータを付け加える（配列の一番最後に新しく一つ付け加えてそこにデータを入れる）。（）の中身が付け加えるデータ。
-	//array.data[i](iは任意)= p（pは任意）;としてもうまく行かない。とりあえず今回は9,3,4,7という４つのデータを含んだデータをMultiArrayInt32型のarrayのデータに入れる。。
+//	array.data.push_backでarrayデータの最後尾にデータを付け加える（配列の一番最後に新しく一つ付け加えてそこにデータを入れる）。（）の中身が付け加えるデータ。
+//	array.data[i](iは任意)= p（pは任意）;としてもうまく行かない。とりあえず今回は9,3,4,7という４つのデータを含んだデータをMultiArrayInt32型のarrayのデータに入れる。。
         array.data.push_back(9);
         array.data.push_back(3);
         array.data.push_back(4);
         array.data.push_back(7);
-        //Publish array 　　arrayをパブする。
+//	Publish array 　　arrayをパブする。
         pub.publish(array);
-        //Let the world know   ターミナル上にプリントする。ここでは「I published something!」が表示される。
+//	Let the world know   ターミナル上にプリントする。ここでは「I published something!」が表示される。
         ROS_INFO("I published something!");
-        //Do this.
+//	Do this.
         ros::spinOnce();
-        //Added a delay so not to spam　左記の通り
+//	Added a delay so not to spam　左記の通り
         sleep(2);
     }
 
