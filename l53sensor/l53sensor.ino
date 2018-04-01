5e9f570d96461bd1db71e819e6096e99f4bc67a3
gagne <communicate.cd7b@gmail.com>
2018-04-01 20:26:54 +0900

    vl53sensorの修正

--- l53sensor/l53sensor.ino
+++ l53sensor/l53sensor.ino
@@ -9,8 +9,8 @@ VL53L0X sensorR;
 VL53L0X sensorL;
 int L_distance,R_distance;
  
-#define RSHUT 6
-#define LSHUT 9
+#define LSHUT 6
+#define RSHUT 9
 
 double getAngle(int l, int r){
   double a = l-r;
@@ -20,13 +20,32 @@ double getAngle(int l, int r){
 }
  
 void setup() {
-pinMode(13,OUTPUT);
-digitalWrite(13,HIGH);
-pinMode(RSHUT, OUTPUT);
-pinMode(LSHUT, OUTPUT);
-digitalWrite(RSHUT, LOW);
-digitalWrite(LSHUT, LOW);
-#if defined LONG_RANGE // ⇐なぜかロングレンジにならない
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  pinMode(RSHUT, OUTPUT);
+  pinMode(LSHUT, OUTPUT);
+  digitalWrite(RSHUT, LOW);
+  digitalWrite(LSHUT, LOW);
+ 
+  delay(100);
+  Wire.begin();
+ 
+  pinMode(LSHUT, INPUT);
+  delay(150);
+  sensorL.init(true);
+  delay(100);
+  sensorL.setAddress((uint8_t)24);
+  sensorL.setTimeout(500);
+
+ 
+  pinMode(RSHUT, INPUT);
+  delay(150);
+  sensorR.init(true);
+  delay(100);
+  sensorR.setAddress((uint8_t)23);
+  sensorR.setTimeout(500);
+
+  #if defined LONG_RANGE //2200くらいまでレンジを広げる
   // lower the return signal rate limit (default is 0.25 MCPS)
   sensorR.setSignalRateLimit(0.1);
   // increase laser pulse periods (defaults are 14 and 10 PCLKs)
@@ -34,45 +53,26 @@ digitalWrite(LSHUT, LOW);
   sensorR.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
     // lower the return signal rate limit (default is 0.25 MCPS)
   sensorL.setSignalRateLimit(0.1);
-  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+   //increase laser pulse periods (defaults are 14 and 10 PCLKs)
   sensorL.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
   sensorL.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
 #endif
  
-delay(100);
-Wire.begin();
+  sensorL.startContinuous();
+  sensorR.startContinuous();
+  digitalWrite(13,LOW);
  
-pinMode(LSHUT, INPUT);
-delay(150);
-sensorL.init(true);
-delay(100);
-sensorL.setAddress((uint8_t)24);
-sensorL.setTimeout(500);
-
- 
-pinMode(RSHUT, INPUT);
-delay(150);
-sensorR.init(true);
- 
-delay(100);
-sensorR.setAddress((uint8_t)23);
-sensorR.setTimeout(500);
- 
-sensorL.startContinuous();
-sensorR.startContinuous();
-digitalWrite(13,LOW);
- 
-Serial.begin(19200);
+  Serial.begin(19200);
 }
  
 void loop(){
   R_distance = sensorR.readRangeContinuousMillimeters();
   L_distance = sensorL.readRangeContinuousMillimeters();
-Serial.print("R:");
-Serial.print(R_distance);
-Serial.print(" L:");
-Serial.print(L_distance);
-Serial.print("  Angle");
-Serial.println(getAngle(L_distance,R_distance));
-delay(100);
+  Serial.print("R:");
+  Serial.print(R_distance);
+  Serial.print(" L:");
+  Serial.print(L_distance);
+  Serial.print("  Angle");
+  Serial.println(getAngle(L_distance,R_distance));
+  delay(100);
 }
