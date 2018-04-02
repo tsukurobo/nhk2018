892547814c12c234a84885ec68c8b687437c9cd1
syatch <sket.dance.again@gmail.com>
2018-04-03 05:18:50 +0900

    複数の色を見られるようにした

--- image_processing/convert/src/convert.cpp
+++ image_processing/convert/src/convert.cpp
@@ -13,9 +13,9 @@
 #include "std_msgs/Int32MultiArray.h"
 #include <stdlib.h>
 static const std::string OPENCV_WINDOW = "Image window";
-static int count;
-float  x;
-float y;
+static int count[2];
+float  x[2];
+float y[2];
 class ImageConverter
 {
   ros::NodeHandle nh_;
@@ -63,7 +63,7 @@ ImageConverter()
     }
 
     //（使うものの宣言？）
-    cv::Mat hsv_image, color_mask;
+    cv::Mat hsv_image, color_mask1, color_mask2;
 
     // cv_ptrをRGB表色系からHSV表色系へ変換して、hsv_imageに格納
     cv::cvtColor(cv_ptr->image, hsv_image, CV_BGR2HSV);
@@ -73,10 +73,11 @@ ImageConverter()
 
 
 
-    cv::Rect range(50, 50, 100, 100);
+    cv::Rect range(10, 10, 500, 450);
     //画像のトリミング範囲を指定
     //hsv_imageのトリミング済みのものをhsv_image_tに格納
 	cv::Mat hsv_ch=hsv_image(range);
+	cv::Mat cv_image1;
 	cv::Mat cv_image2;
 
 
@@ -85,44 +86,57 @@ ImageConverter()
     // hsv=｛色相(Hue), 彩度(Saturation), 明暗(Value, brightness)｝ 
     // 指定した範囲の色でマスク画像color_mask(CV_8U:符号なし8ビット整数)を生成  
     // マスク画像は指定した範囲の色に該当する要素は255(8ビットすべて1)、それ以外は0                                                    
-    cv::inRange(hsv_ch, cv::Scalar(75, 0, 40, 0) , cv::Scalar(165, 255, 255, 0), color_mask);
+    cv::inRange(hsv_ch, cv::Scalar(10, 0, 130, 0) , cv::Scalar(40, 255, 255, 0), color_mask1);
+    cv::inRange(hsv_ch, cv::Scalar(95,45, 40, 100) , cv::Scalar(125, 255, 255, 0), color_mask2);
     //hsv_chに対し、（ｈ色相,ｓ彩度,ｖ明度）(150,100,50,0)から(180,255,255,0)の範囲でマスク画像color_mask1を生成(cv::inRangeの作用として、マスク画像は該当する要素は255,それ以外は0?)
 	
-	count = cv::countNonZero(color_mask);
+	count[0] = cv::countNonZero(color_mask1);
 	
-	cv::Mat img = color_mask.clone();
-	cv::Moments mu = moments(img,false);
-	x= mu.m10/mu.m00;
-	y=mu.m01/mu.m00;
+	cv::Mat img1 = color_mask1.clone();
+	cv::Moments mu1 = moments(img1,false);
+	x[0]= mu1.m10/mu1.m00;
+	y[0]=mu1.m01/mu1.m00;
+	
+	count[1] = cv::countNonZero(color_mask2);
+	
+	cv::Mat img2 = color_mask2.clone();
+	cv::Moments mu2 = moments(img2,false);
+	x[1]= mu2.m10/mu2.m00;
+	y[1]=mu2.m01/mu2.m00;
 
 	
     // ビット毎の論理積。マスク画像は指定した範囲以外は0で、指定範囲の要素は255なので、ビット毎の論理積を適用すると、指定した範囲の色に対応する要素はそのままで、他は0になる。
-    cv::bitwise_and(hsv_ch, hsv_ch, cv_image2, color_mask);
+    cv::bitwise_and(hsv_ch, hsv_ch, cv_image1, color_mask1);
+    cv::bitwise_and(hsv_ch, hsv_ch, cv_image2, color_mask2);
     //マスク画像とcv_ptrに対し、ビットごとに論理積を適用。これをcv_image2に格納
 
 
 //各画像のサイズを変更
 
     // 画像サイズ指定
-    cv::Mat cv_half_image, cv_half_image2;
-    cv::resize(cv_ptr->image, cv_half_image,cv::Size(),1.5,1.5);
+    cv::Mat cv_half_image, cv_half_image1, cv_half_image2;
+    cv::resize(cv_ptr->image, cv_half_image,cv::Size(),1.5,1.5);  
+    cv::resize(cv_image1, cv_half_image1,cv::Size(),1.5,1.5);
     cv::resize(cv_image2, cv_half_image2,cv::Size(),1.5,1.5);
 
 //ウィンドウ表示
 
     // ウインドウ表示                                                                         
     cv::imshow("Original Image", cv_half_image);
-    cv::imshow("Result Image", cv_half_image2);
+    cv::imshow("Result Image1", cv_half_image1);
+    cv::imshow("Result Image2", cv_half_image2);
     cv::waitKey(3);
 
 
 	std_msgs::Int32MultiArray image_param;
 	image_param.data.clear();
-	image_param.data.push_back(x);
-	image_param.data.push_back(y);
-	image_param.data.push_back(count);
+	for(int j=0;j<2;j++){
+		image_param.data.push_back(x[j]);
+		image_param.data.push_back(y[j]);
+		image_param.data.push_back(count[j]);
+	}
 	pub.publish(image_param);
-	ROS_INFO("x=%f,y=%f,value=%d",x,y,count);
+	ROS_INFO("x=%f,y=%f,value=%d,x=%f,y=%f,value=%d",x[0],y[0],count[0],x[1],y[1],count[1]);
 
   }
 };
