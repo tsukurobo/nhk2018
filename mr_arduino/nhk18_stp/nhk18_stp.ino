5bbdbc3986487cc4066b5a6f26d9304979cb253a
gagne <communicate.cd7b@gmail.com>
2018-04-04 19:00:58 +0900

    stp

--- mr_arduino/nhk18_stp/nhk18_stp.ino
+++ mr_arduino/nhk18_stp/nhk18_stp.ino
@@ -1,41 +1,25 @@
-#include <Stepper.h>
 #include <ros.h>
 #include <std_msgs/Int8.h>
 
 #define WHEEL_DIAMETER 40//だいたい
 #define STEP 1.8
-#define STEPS 200
-#define PW 100
+#define STEPS 200// 360 / STEP (360度の回転に何パルス必要か
 #define RPM 30 //分あたりの回転数
 #define PULSE_ROLL (WHEEL_DIAMETER * 3.14 * STEP / 360)//１パルスあたり
+#define STEPPING_MOTOR_SUM 2
+#define DEFAULTPW 80
 
-static const int motor_cw[2] = {3, 4};
-static const int motor_ccw[2] = {2, 5};
-
-Stepper stepper_a(STEPS, motor_cw[0], motor_ccw[0]);
-Stepper stepper_b(STEPS, motor_cw[1], motor_ccw[1]);
 
 void stpaCallback(const std_msgs::Int16& a){
      int input_pulse = a.data/PULSE_ROLL;
-     stepper_a.step(input_pulse);
 }
 void stpbCallback(const std_msgs::Int16& b){
      int input_pulse = b.data/PULSE_ROLL;
-     stepper_b.step(input_pulse);
-
 }
+
 ros::NodeHandle nh;
 ros::Subscriber<std_msgs::Int16> stpa("stpa",stpaCallback);
 ros::Subscriber<std_msgs::Int16> stpb("stpb",stpbCallback);
-
-void stp_motor_init(){
-  for(int i = 0; i < 2; i++){
-    pinMode(motor_cw[i], OUTPUT);
-    pinMode(motor_ccw[i], OUTPUT);
-  }
-  stepper_a.setSpeed(RPM);
-  stepper_b.setSpeed(RPM);
-}
 
 void setup(){
   nh.initNode();
